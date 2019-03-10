80ee51b2d2310bd9caaf2e16447e55cd2cbd4bb8
ina <devnull@localhost>
2019-03-10 15:24:46 +0100

    delete config.py (with local settings) from repository

--- src/ELDAmwl/configs/config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# from .config_default import *
-
-
-# ===================
-# user specific configurations in this file
-# override configs in config_default.py
-# ===================
