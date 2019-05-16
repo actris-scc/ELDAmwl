6d6beb38a7e53192863221d5c3b24f874acc2db1
volker <volker.jaenisch@inqbus.de>
2019-05-16 16:06:44 +0200

    test for MeasurementParams

---
+++ src/ELDAmwl/tests/test_elda_mwl_factories.py
@@ -0,0 +1,28 @@
+from attrdict import AttrDict
+
+from ELDAmwl.elda_mwl_factories import MeasurementParams
+
+def test_MeasurementParams(mocker):
+    # REALLY important! Patch where the function is imported not where the function is defined!!!!!!
+    mocker.patch('ELDAmwl.elda_mwl_factories.read_system_id', return_value=0)
+    mocker.patch('ELDAmwl.elda_mwl_factories.read_mwl_product_id', return_value=1)
+
+    val = AttrDict({'Products': AttrDict({'ID': 1})})
+
+    mocker.patch('ELDAmwl.elda_mwl_factories.get_products_query', return_value=[val])
+
+    measurement_params = MeasurementParams(4)
+    measurement_params.read_product_list()
+
+    assert measurement_params.system_id == 0
+
+    assert measurement_params.mwl_product_id == 1
+    assert measurement_params.products[0].prod_id == 1
+
+    assert measurement_params.meas_id == 4
+
+    pass
+
+
+
+
