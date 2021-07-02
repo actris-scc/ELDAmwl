e78e01f7806bc0ebd12b75b5197deb916dc23d7d
ina <devnull@localhost>
2021-07-02 15:18:24 +0200

    preliminary code for testing database for duplicate entries in lidar_constants table

--- src/ELDAmwl/database/tables/lidar_constants.py
+++ src/ELDAmwl/database/tables/lidar_constants.py
@@ -42,3 +42,52 @@ class LidarConstants(Base):
     is_latest_value = Column(INTEGER, nullable=False)
 
 
+"""
+from ELDAmwl.database.tables.lidar_constants import LidarConstants
+from sqlalchemy import create_engine
+try:
+    import ELDAmwl.configs.config as cfg
+except ImportError:
+    import ELDAmwl.configs.config_default as cfg
+    
+connect_str = 'mysql+pymysql://{user}:{passw}@{host}/{db}'.format(
+            user=cfg.DB_USER,
+            passw=cfg.DB_PASS,
+            host=cfg.DB_SERVER,
+            db=cfg.DB_DB)
+engine = create_engine(connect_str)
+
+conn = engine.connect()
+
+res = conn.execute('select * from (SELECT __measurements__ID, _product_ID, _hoi_system_ID, _channel_ID, profile_start_time, profile_end_time, count(*)  as count FROM lidar_constants WHERE is_latest_value=1 group by  __measurements__ID, _product_ID, _hoi_system_ID, _channel_ID, profile_start_time, profile_end_time) as selectcount where selectcount.count >1 order by __measurements__ID;')
+duplicates = res.fetchall()
+
+del_ids = []
+for row in duplicates:
+    statement=('select id, InscribedAt from lidar_constants '
+                 'where __measurements__ID = "{}" and '
+                 '_product_ID = {} and '
+                 '_hoi_system_ID={} and '
+                 '_channel_ID={} and '
+                 'profile_start_time = "{}" and '
+                 'profile_end_time = "{}" and '
+                 'is_latest_value = 1 order by InscribedAt desc;').format(row[0], 
+                                                                          row[1], 
+                                                                          row[2], 
+                                                                          row[3], 
+                                                                          row[4], 
+                                                                          row[5])
+    #print(statement)
+    res = conn.execute(statement)
+    for line in res.fetchall()[1:]:
+        #print(row[0],line)
+        del_ids.append(line[0])
+
+outfile = open('d:/temp/delete_ids.txt', 'w')
+for id in del_ids:
+    outfile.write('{} '.format(id))
+    
+outfile.close()
+
+
+"""
