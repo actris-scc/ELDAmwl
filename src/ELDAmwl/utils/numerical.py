Fixed off by one error

src/ELDAmwl/utils/numerical.py
@@ -49,8 +49,7 @@ def calc_minimal_window_indexes(means, sems, w_width, error_threshold):
                    rel_sem * means)
 
     # min_idx is the last bin of rolling window with smallest mean
-    min_idx = np.nanargmin(valid_means.data, axis=1)
-    win_last_idx = min_idx + 1
+    win_last_idx = np.nanargmin(valid_means.data, axis=1)
     win_first_idx = (win_last_idx[:] - w_width[:, 0])
 
     return win_first_idx, win_last_idx