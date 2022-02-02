d9250778440c3615ae333601f9cf6671109727e9
inamattis <ina@ina-mattis.de>
2022-02-02 21:40:03 +0100

    add database table hoi_telescopes

--- ELDAmwl/database/tables/channels.py
+++ ELDAmwl/database/tables/channels.py
@@ -4,7 +4,7 @@
 from ELDAmwl.database.tables.db_base import Base
 from sqlalchemy import Column
 from sqlalchemy import DECIMAL
-from sqlalchemy import INTEGER
+from sqlalchemy import INTEGER, FLOAT, DateTime
 from sqlalchemy import String
 from sqlalchemy import text
 
@@ -132,4 +132,94 @@ class Channels(Base):
         String(5),
         nullable=False,
         server_default=text("'0'"),
+    )
+
+class Telescopes(Base):
+    __tablename__ = 'hoi_telescopes'
+
+    ID = Column(
+        INTEGER,
+        primary_key=True,
+    )
+    type = Column(
+        String(100),
+        nullable=False,
+        server_default=text("''"),
+    )
+    diameter = Column(
+        FLOAT,
+        nullable=False,
+        server_default=text("'0'"),
+    )
+    focal_length = Column(
+        FLOAT,
+        nullable=False,
+        server_default=text("'0'"),
+    )
+    full_overlap_height_m = Column(
+        FLOAT,
+        nullable=False,
+        server_default=text("'0'"),
+    )
+    manufacturer = Column(
+        String(100),
+        nullable=True,
+    )
+    model = Column(
+        String(100),
+        nullable=True,
+    )
+    obscuration_diameter = Column(
+        FLOAT,
+        nullable=True,
+    )
+    field_of_view = Column(
+        FLOAT,
+        nullable=True,
+    )
+    field_stop_type = Column(
+        String(45),
+        nullable=True,
+    )
+    field_stop_size = Column(
+        FLOAT,
+        nullable=True,
+    )
+    optical_fiber_num_aperture = Column(
+        FLOAT,
+        nullable=True,
+    )
+    optical_fiber_manufacturer = Column(
+        String(100),
+        nullable=True,
+    )
+    optical_fiber_type = Column(
+        String(100),
+        nullable=True,
+    )
+    collimation_focal_length = Column(
+        FLOAT,
+        nullable=True,
+    )
+    entry_update_date = Column(
+        DateTime,
+        nullable=False,
+    )
+    collimation_type = Column(
+        String(100),
+        nullable=True,
+    )
+    collimation_model = Column(
+        String(100),
+        nullable=True,
+    )
+    station_id = Column(
+        '__hoi_stations__ID',
+        INTEGER,
+        nullable=True,
+    )
+    locked = Column(
+        INTEGER,
+        nullable=False,
+        server_default=text("'0'")
     )
