0f10727b29f288e826987656b26d3b365e865d43
inamattis <ina@ina-mattis.de>
2023-10-19 19:40:36 +0200

    savitsky-golay params in cache on harddisc.

--- ELDAmwl/storage/cached_functions.py
+++ ELDAmwl/storage/cached_functions.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 """functions which are often called with same parameters. Their results can be cached"""
+import pickle
 
 from ELDAmwl.component.interface import IDBFunc
 from functools import lru_cache
 from scipy.signal import savgol_coeffs
 from zope import component
 
+DEFAULT_ORDER = 2
+SG_PARAMS_FILENAME = 'sg_params.pickle'
 
-@lru_cache(maxsize=100)
+def gen_sg_params():
+    sg_param = {}
+    for window_length in range(10, 100):
+        sg_param[window_length]=savgol_coeffs(window_length, DEFAULT_ORDER)
+    with open(SG_PARAMS_FILENAME, 'wb') as outfile:
+        pickle.dump(sg_param, outfile)
+
+try:
+    with open(SG_PARAMS_FILENAME, 'rb') as infile:
+        SG_PARAMS = pickle.load(infile)
+except Exception as e:
+    gen_sg_params()
+
+
 def sg_coeffs(window_length, order):
-    return savgol_coeffs(window_length, order)
+    return SG_PARAMS[window_length]
 
 
 @lru_cache(maxsize=100)
